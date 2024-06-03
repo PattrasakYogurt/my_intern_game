9a593029fe8178b7f096f6cb952eb7ac6a7cd836
PattrasakYogurt <pattrasak.chi@spumail.net>
2024-06-03 19:50:49 +0700

    only script

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> enemyList;
+    [SerializeField] private List<Transform> spawnPointList;
+    [SerializeField] private float spawnTime;
+    [SerializeField] private float timeSpawnInterval;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
