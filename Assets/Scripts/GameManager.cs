a6779e0726313978c93c2716dbfc50796e18eda2
PattrasakYogurt <pattrasak.chi@spumail.net>
2024-06-03 14:56:36 +0700

    player move

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -43,10 +43,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if(Input.GetKeyUp(KeyCode.Escape))
         {            
                 PauseGame();           
         }
+        */
     }
     public void DisablePlayer()
     {
