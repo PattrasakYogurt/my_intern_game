6fff7acc949c24d2e106bd04d9d0a00c443b3cfd
PattrasakYogurt <pattrasak.chi@spumail.net>
2024-06-04 16:03:36 +0700

    enemy script part 2

--- Assets/Scripts/Enemy/EnemyAi.cs
+++ Assets/Scripts/Enemy/EnemyAi.cs
@@ -2,45 +2,103 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
-enum state
-{
-    idle = 0,
-    walk = 1,
-    run = 2,
-    attack = 3,    
-}
 public class EnemyAi : MonoBehaviour
 {
-    public Transform target;
-    [SerializeField] private float walkSpeed = 5f;
-    [SerializeField] private float runSpeed = 10f;
-    [SerializeField] private float detectedDistance = 10f;
-    [SerializeField] private float forceDetectedDistance = 4f;
-    [SerializeField] private float detectionAngle = 90f;
-    [SerializeField] private float huntTime;
-    [SerializeField] private float huntTimeCount;
-    [SerializeField] private Animator animator;
-    [SerializeField] private NavMeshAgent agent;
-    [SerializeField] private AudioSource detectedSound;
-    [SerializeField] private AudioSource attackSound;
-    [SerializeField] private AudioSource runBGM;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer; 
+    public NavMeshAgent agent;
+    [Header("walk")]
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+    [Header("Attack")]
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    [Header("States")]
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
     [SerializeField] private PlayerController playerController;
-    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+    }
     void Start()
     {
-        target = GameObject.FindWithTag("Player").transform;
+        
         playerController = GetComponent<PlayerController>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        if(!playerInSightRange && !playerInAttackRange)
+        {
+            Patroling();
+        }
+        if(playerInSightRange && !playerInAttackRange)
+        {
+            ChasePlayer();
+        }
+        if(playerInSightRange && playerInAttackRange)
+        {
+            AttackPlayer();
+        }
     }
-    
-    public void UpdateState()
+    private void SearchWalkPoint()
     {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x, transform.position.y, transform.position.z + randomZ);
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
 
+    }
+    private void Patroling()
+    {
+        if(!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+        Vector3 distanceWalkPoint = transform.position - walkPoint;
+        if(distanceWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+        if(!alreadyAttacked)
+        {
+            playerController.TakeDamage(20);
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
