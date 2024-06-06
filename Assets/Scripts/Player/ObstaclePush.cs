715dc39144df33fed7ebca8f0132c830f06debbc
PattrasakYogurt <pattrasak.chi@spumail.net>
2024-06-06 12:53:52 +0700

    test push

--- Assets/Scripts/Player/ObstaclePush.cs
+++ Assets/Scripts/Player/ObstaclePush.cs
@@ -18,14 +18,19 @@ public class ObstaclePush : MonoBehaviour
     }
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Rigidbody rigibody = hit.collider.attachedRigidbody;
-        if (rigibody != null)
+        if (hit.collider.tag == "PushObject")
         {
-            Vector3 forceDirection = hit.gameObject.transform.position - transform.position;
-            forceDirection.y = 0;
-            forceDirection.Normalize();
 
-            rigibody.AddForceAtPosition(forceDirection * forceMagnitude, transform.position, ForceMode.Impulse);
+
+            Rigidbody rigibody = hit.collider.attachedRigidbody;
+            if (rigibody != null)
+            {
+                Vector3 forceDirection = hit.gameObject.transform.position - transform.position;
+                forceDirection.y = 0;
+                forceDirection.Normalize();
+
+                rigibody.AddForceAtPosition(forceDirection * forceMagnitude, transform.position, ForceMode.Impulse);
+            }
         }
     }
 }
