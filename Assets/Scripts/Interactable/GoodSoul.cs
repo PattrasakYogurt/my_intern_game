5931cfcf382122f737decfa6497a59f24bbb2d62
PattrasakYogurt <pattrasak.chi@spumail.net>
2024-07-05 14:42:44 +0700

    move

--- Assets/Scripts/Interactable/GoodSoul.cs
+++ Assets/Scripts/Interactable/GoodSoul.cs
@@ -6,6 +6,9 @@ public class GoodSoul : MonoBehaviour , IInteractable
 {
     [SerializeField] private PlayerController playerController;
     [SerializeField] private AudioSource soulSound;
+    public bool isUse = false;
+    public ParticleSystem particle;
+
     public string GetInteractionText()
     {
         return "Press E to get";
@@ -14,8 +17,15 @@ public class GoodSoul : MonoBehaviour , IInteractable
     public void Interact()
     {
         playerController.GetComponent<PlayerController>();
-        playerController.Heal(20);
+        if(isUse == false)
+        {
+           playerController.Heal(20);
+            isUse = true;
+        }
         soulSound.Play();
+        ParticleSystem spawnParticle = Instantiate(particle, transform.position, Quaternion.identity);
+        spawnParticle.Play();
+        Destroy(spawnParticle.gameObject,spawnParticle.main.duration + spawnParticle.main.startLifetime.constantMax);
         Destroy(gameObject, 0.5f);
     }
 
