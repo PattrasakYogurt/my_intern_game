2446026e1e4924ed89453a556ca76504d283887a
PattrasakYogurt <pattrasak.chi@spumail.net>
2024-07-09 16:57:39 +0700

    puzzleImprove

--- Assets/Scripts/PushPuzzleCheck.cs
+++ Assets/Scripts/PushPuzzleCheck.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class PushPuzzleCheck : MonoBehaviour
 {
     public GameObject puzzle;
+    public GameObject cubeCheck;
+    public ParticleSystem correctParticles;
     
     // Start is called before the first frame update
     void Start()
     {
         puzzle.SetActive(true);
+        correctParticles.Stop();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ObjectForPush"))
         {
             puzzle.SetActive(false);
+            correctParticles.Play();
+            Destroy(cubeCheck, 2.1f);
         }
     }
 }
