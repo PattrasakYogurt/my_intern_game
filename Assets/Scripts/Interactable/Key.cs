b2fed65ed9e0c5f65e27bed8c093e35bbbe281ae
PattrasakYogurt <pattrasak.chi@spumail.net>
2024-07-19 11:23:07 +0700

    move

--- Assets/Scripts/Interactable/Key.cs
+++ Assets/Scripts/Interactable/Key.cs
@@ -6,7 +6,13 @@ using UnityEngine.UI;
 public class Key : MonoBehaviour, IInteractable
 {
     [SerializeField] private AudioSource keySound;
+    public ParticleSystem key_particle_keep;
     bool isKeep = false;
+
+    void Start()
+    {
+        key_particle_keep.Stop();
+    }
     public string GetInteractionText()
     {
         return "Press E to collect";
@@ -19,6 +25,7 @@ public class Key : MonoBehaviour, IInteractable
             isKeep = true;
             GameManager.instance.keyObtained += 1;
             keySound.Play();
+            key_particle_keep.Play();
         }           
         Destroy(gameObject, 1f);
     }
